a4daecfe3f4aa386773a65a657b726cdf3ec3400
shahril <arel_lonelyslumber@yahoo.com>
2016-04-12 02:13:10 +0800

    testing lagi

--- views/site/about.php
+++ views/site/about.php
@@ -11,7 +11,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        This is SHAHRIL ANUAR bin md din bin mawi:
+        This is SHAHRIL ANUAR bin md din bin mawi adad:
     </p>
 
     <code><?= __FILE__ ?></code>
